17551dd0bf696756066177c601a072667c7637fa
BattaMahendra <BMC26G@GMAIL.COM>
2024-06-06 15:54:41 +0530

    Basics of mvc and boot

--- services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+
 @SpringBootApplication
 public class ProductDetailsServiceApplication {
 
@@ -11,6 +13,7 @@ public class ProductDetailsServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ProductDetailsServiceApplication.class, args);
+
 	}
 
 }
