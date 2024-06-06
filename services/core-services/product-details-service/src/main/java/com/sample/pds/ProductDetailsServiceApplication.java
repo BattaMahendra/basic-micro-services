ebd0c46d429ad5e7753fd66fd1600ee1933f3fad
BattaMahendra <BMC26G@GMAIL.COM>
2024-06-06 14:15:09 +0530

    Basics of spring boot

--- services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
@@ -1,12 +1,15 @@
 package com.sample.pds;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductDetailsServiceApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductDetailsServiceApplication.class, args);
 	}
 
