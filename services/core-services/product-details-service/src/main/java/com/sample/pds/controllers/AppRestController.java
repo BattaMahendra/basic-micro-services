496ef57257acdf93d552735d8a1dc5c235f0da2d
BattaMahendra <BMC26G@GMAIL.COM>
2024-06-07 19:56:14 +0530

    Creating simple spring boot app to experiment and learn

--- services/core-services/product-details-service/src/main/java/com/sample/pds/controllers/AppRestController.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/controllers/AppRestController.java
@@ -1,7 +1,34 @@
 package com.sample.pds.controllers;
 
+import com.sample.pds.entity.Employee;
+import com.sample.pds.repositories.EmployeeRepository;
+import com.sample.pds.services.AppService;
+import com.sample.pds.services.implementation.AppServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.awt.*;
+import java.util.List;
 
 @RestController //==> @controller + @ResponseBody
 public class AppRestController {
+
+    @Autowired
+    AppServiceImpl appService;
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployee(){
+        return appService.getAllEmployees();
+    }
+
+    //this controller accepts json and returns xml
+    @PostMapping(value = "/addE",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_XML_VALUE)
+    public Employee addAnEmployee(@RequestBody Employee employee){
+        return appService.addAnEmployee(employee);
+    }
 }
--- services/core-services/product-details-service/src/main/java/com/sample/pds/services/implementation/AppServiceImpl.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/services/implementation/AppServiceImpl.java
@@ -2,9 +2,11 @@ package com.sample.pds.services.implementation;
 
 import com.sample.pds.entity.Employee;
 import com.sample.pds.repositories.EmployeeRepository;
+import com.sample.pds.services.AppService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -12,7 +14,18 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class AppServiceImpl {
+public class AppServiceImpl implements AppService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee addAnEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
 
 
 
