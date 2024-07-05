e89a7f6f4c86ffa9659e0d61f73f92f609646eb1
BattaMahendra <BMC26G@GMAIL.COM>
2024-07-05 08:12:01 +0530

    Adding okta oauth2.0 security

--- services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/ProductDetailsServiceApplication.java
@@ -2,12 +2,15 @@ package com.sample.pds;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.HashSet;
 
 @SpringBootApplication
+
 @ComponentScan(basePackages = {"com.sample.pds"})
 public class ProductDetailsServiceApplication {
 
---
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/configuration/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.sample.pds.configuration;
+
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public  class SecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
+}
