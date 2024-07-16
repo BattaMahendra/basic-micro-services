be3f3ea524c05ce7d6251868b1e08b49f764eea0
BattaMahendra <BMC26G@GMAIL.COM>
2024-07-16 20:04:08 +0530

    Learning @Configuration

--- services/core-services/product-details-service/src/main/java/com/sample/pds/configuration/Config.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/configuration/Config.java
@@ -23,6 +23,10 @@ public class Config {
     @Qualifier("sendOffBean")
     @Profile("dev")
     public java.lang.String sendOffString(){
+        System.out.println("config class bean address "+System.identityHashCode(testingConfigurationClass()));
+        System.out.println("config class bean address "+System.identityHashCode(testingConfigurationClass()));
+
+
         return "Bye bye Bean";
     }
 
@@ -30,6 +34,26 @@ public class Config {
     @Qualifier("testingBean")
     @Profile("!dev")     //it will be active in all non dev profiles
     public java.lang.String testInProcessString(){
+
         return "test Bean";
+    }
+
+
+    /*
+    *
+    * When we create beans in class annotated with @Configuration class then it will always
+    * return singleton instance. That is the benefit of creating beans in configuration classes
+    * That is because
+    * @Configuration classes are enhanced by Spring using CGLIB proxies.
+    * This enhancement ensures that each @Bean method is called only once and that the same instance (singleton) is returned every time.
+    *
+    *
+    * you can check this by creating beans in classes annotated with @Component
+    * you can check that in controller package where I have coded.
+    * */
+    @Bean
+    public String testingConfigurationClass(){
+        System.out.println("We are in configuration class");
+        return new String("new");
     }
 }
--- services/core-services/product-details-service/src/main/java/com/sample/pds/controllers/AppController.java
+++ services/core-services/product-details-service/src/main/java/com/sample/pds/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.sample.pds.controllers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController
 {
+
+
+    /*
+    *
+    * You can create beans in @Component classes also
+    * but here you will have disadvantage, whenever same bean is called in the same class then
+    * instead of returning same bean it will create new bean every time
+    *
+    * but if you declare class with @Configuration class then this problem will not occur
+    * it will always return same instance. Means the default scope of bean will always be singleton
+    *
+    * */
+    @Bean
+    public String testComponentClass(){
+        System.out.println("We are in controller class");
+        return new String("news");
+    }
+
+    @Bean
+    public int testAboveAddress(){
+        System.out.println("controller class bean address "+System.identityHashCode(testComponentClass()));
+        System.out.println("controller class bean address "+System.identityHashCode(testComponentClass()));
+
+        return 1;
+    }
 
     @GetMapping("/welcome")
     public String welcome(){
